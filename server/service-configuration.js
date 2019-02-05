18fb8be013d84a30fd0e2a46dd8f24f9e1481ece
Dmitriy Valkov <dlcwalkoff@gmail.com>
2019-02-05 11:39:31 +0300

    fix: right configs keys

--- server/service-configuration.js
+++ server/service-configuration.js
@@ -1,39 +1,29 @@
 ServiceConfiguration.configurations.upsert({
     service: "facebook"
-  }, {
+}, {
     $set: {
-      appId: Meteor.settings.facebook.appId,
-      loginStyle: "popup",
-      secret: Meteor.settings.facebook.secret
+        appId: Meteor.settings.facebook.appId,
+        loginStyle: "popup",
+        secret: Meteor.settings.facebook.secret
     }
-  });
+});
   
-  ServiceConfiguration.configurations.upsert({
+ServiceConfiguration.configurations.upsert({
     service: "github"
-  }, {
+}, {
     $set: {
-      appId: Meteor.settings.github.appId,
-      loginStyle: "popup",
-      secret: Meteor.settings.github.secret
+        clientId: Meteor.settings.github.clientId,
+        loginStyle: "popup",
+        secret: Meteor.settings.github.secret
     }
-  });
+});
   
-  ServiceConfiguration.configurations.upsert({
-    service: "twitter"
-  }, {
-    $set: {
-      appId: Meteor.settings.twitter.appId,
-      loginStyle: "popup",
-      secret: Meteor.settings.twitter.secret
-    }
-  });
-  
-  ServiceConfiguration.configurations.upsert({
+ServiceConfiguration.configurations.upsert({
     service: "google"
-  }, {
+}, {
     $set: {
-      appId: Meteor.settings.google.appId,
-      loginStyle: "popup",
-      secret: Meteor.settings.google.secret
+        clientId: Meteor.settings.google.clientId,
+        loginStyle: "popup",
+        secret: Meteor.settings.google.secret
     }
-  });
+});
